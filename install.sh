5fbc0b5e9826bd3cd9fbc102af16e7a780263f08
hitimr <hitimr@gmail.com>
2021-05-02 23:34:30 +0200

    removed personal data

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 # basic stuff
+
+
+#config
+#git uncomment the lines below to set the git user name
+gitUser="REPLACE ME"
+gitMail="replace@me.com"
+
 sudo apt update
 sudo apt upgrade
-sudo apt install cmake git vlc thunderbird steam onedrive make conky-all
+sudo apt install cmake git vlc thunderbird steam onedrive make conky-all pinta
 sudo snap install --classic code
 sudo snap install zoom-client
 
@@ -50,8 +57,8 @@ jupyter nbextension enable --user --py ngsolve
 
 # Git
 sudo apt-get install git -y
-git config --global user.name hitimr
-git config --global user.email hitimr@gmail.com
+#git config --global user.name $gitUser
+#git config --global user.email $gitMail
 sudo apt install cmake vlc thunderbird steam zoom google-chrome-stable onedrive
 sudo snap install --classic code
 
