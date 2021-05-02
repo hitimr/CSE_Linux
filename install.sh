04886871ef087b2912611a2fe5515faa31177765
hitimr <hitimr@gmail.com>
2021-05-02 03:17:59 +0200

    switched back to shell script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt upgrade
+sudo apt install cmake git vlc thunderbird steam zoom google-chrome-stable onedrive
+sudo snap install --classic code
+
+
+sudo apt install python3 python3-pip
+pip install -Ur py_packages.txt
+
+
+sudo apt install gcc openmpi-bin openmpi-common openmpi-doc
+
+code --install-extension ms-vscode.cpptools austin.code-gnu-global aaron-bond.better-comments
+code --install-extension ms-python.python njpwerner.autodocstring janisdd.vscode-edit-csv tomoki1207.pdf
+code --install-extension james-yu.latex-workshop mhutchie.git-graph ms-toolsai.jupyter ms-vscode-remote.remote-ssh 
+
+
+
