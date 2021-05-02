6c944b2be478ac3152c1e56232eaa4c52e5cd175
hitimr <hitimr@gmail.com>
2021-05-02 02:06:17 +0200

    addedd git username and email

---
+++ sys_packages.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt upgrade
+
+# Git
+sudo apt-get install git -y
+git config --global user.name hitimr
+git config --global user.email hitimr@gmail.com
+
+sudo apt install cmake vlc thunderbird steam zoom google-chrome-stable onedrive
+sudo snap install --classic code
+
+
